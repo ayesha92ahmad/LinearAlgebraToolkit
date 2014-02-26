26b87238f6598a3c6dd749cd1168fd3dba6ee0bf
ayesha92ahmad <ayesha92ahmad@yahoo.com>
2014-02-26 23:31:03 +0530

    Create MainPanel.java

---
+++ MainPanel.java
@@ -0,0 +1,20 @@
+package viewController;
+
+import javax.swing.*; 
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+	JLabel l1= new JLabel("Welcome");
+	
+	MainPanel(){
+		this.add(l1);
+	}
+	MainPanel(JLabel l){
+		this.add(l1);
+	}
+	MainPanel(JPanel p){
+		this.add(p);
+	}	
+	
+
+}
